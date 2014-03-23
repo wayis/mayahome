f5b9a1da7561cf4def48b54a6cdda9d390dd6366
Luc LABBE <luc.labbe@inge.neo-soft.fr>
2014-03-23 18:17:50 +0100

    Add Dashboard component with a widget strategy.

--- src/main/webapp/js/controllers/dashboardController.js
+++ src/main/webapp/js/controllers/dashboardController.js
@@ -1,5 +1,17 @@
 angular.module('mayahomeApp').controller('DashboardCtrl', function ($scope, $http, DeviceServices) {
     DeviceServices.list(function(devices) {
         $scope.devices = devices;
-    });
+    }, function(error) {console.log("error: " + error)});
+
+    // Roller-Shutter && Varying Light
+    $scope.upDeviceValue = function(device) {
+        if (device.value < 100) {
+            device.value = device.value + 1;
+        }
+    }
+    $scope.downDeviceValue = function(device) {
+        if (device.value > 0) {
+            device.value = device.value - 1;
+        }
+    }
 });
